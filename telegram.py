58e16428bd361feccb4ebd1e4839bdbc0bfd5498
Александр <florinskiy.y@gmail.com>
2023-12-15 01:47:53 +0300

    изменена версия aiogram

--- telegram.py
+++ telegram.py
@@ -3,13 +3,14 @@ from config import TOKEN
 from aiogram import Bot, Dispatcher
 
 bot = Bot(TOKEN)
-dp = Dispatcher()
+dp = Dispatcher(bot)
 
 
-@dp.message()
+@dp.message_handler()
 async def fnc(text):
+    ses = await bot.get_session()
     await bot.send_message(chat_id="888281527", text=text)
-    await bot.session.close()
+    await ses.close()
 
 
 def send(text):
