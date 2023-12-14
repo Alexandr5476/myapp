0deb0f4f7438eb668df660b54bb760e251de7806
Александр <florinskiy.y@gmail.com>
2023-12-15 02:47:10 +0300

    В spec добавлены скрытые библиотеки

--- telegram.py
+++ telegram.py
@@ -1,13 +1,8 @@
-from config import TOKEN
 import telebot
 
-bot = telebot.TeleBot(TOKEN)
-
-
-def fnc(text):
-    bot.send_message("888281527", text)
+bot = telebot.TeleBot('6654600196:AAEHouKkxE26ltUOMvjou9LYwCuhJl4hR0k')
 
 
 def send(text):
     if text != '':
-        fnc(text)
+        bot.send_message("888281527", text)
